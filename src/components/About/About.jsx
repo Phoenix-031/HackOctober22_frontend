b20582824dbb6ea25441c54b9b8d344bdc1f5635
Debayan <pradhandebayan@gmail.com>
2022-10-30 23:49:11 +0530

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,13 +5,13 @@ const About = () => {
   return (
     <>
     <section id="about" className='overflow-x-scroll'>
-      <div class="wrapper flex ">
-        <div class="carousel owl-carousel flex gap-4">
-          <div class="card card-1 flex justify-center items-center p-10 cursor-pointer">Practice Problems for Exams</div>
-          <div class="card card-2 flex justify-center items-center p-10 cursor-pointer">Complete Study Material</div>
-          <div class="card card-3 flex justify-center items-center p-10 cursor-pointer">University Wise Syllabus</div>
-          <div class="card card-4 flex justify-center items-center p-10 cursor-pointer">Sample Question Papers</div>
-          <div class="card card-5 flex justify-center items-center p-10 cursor-pointer">Previous Years Questions</div>
+      <div className="wrapper flex ">
+        <div className="carousel owl-carousel flex gap-4">
+          <div className="card card-1 flex justify-center items-center p-10 cursor-pointer">Practice Problems for Exams</div>
+          <div className="card card-2 flex justify-center items-center p-10 cursor-pointer">Complete Study Material</div>
+          <div className="card card-3 flex justify-center items-center p-10 cursor-pointer">University Wise Syllabus</div>
+          <div className="card card-4 flex justify-center items-center p-10 cursor-pointer">Sample Question Papers</div>
+          <div className="card card-5 flex justify-center items-center p-10 cursor-pointer">Previous Years Questions</div>
         </div>
       </div>
     </section>
