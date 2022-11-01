7237af776716b009a3d1eb54946bc5ada84c15e6
Debayan <pradhandebayan@gmail.com>
2022-11-01 17:54:28 +0530

    updated

--- src/api/api.filter.js
+++ src/api/api.filter.js
@@ -5,7 +5,7 @@ const filterItems = async (Institute,Year,Semester,Department) => {
     // console.log(FilePath)
     // return (FilePath)
 
-    const response = await axios.post('http://localhost:6723/api/filter/',{
+    const response = await axios.post('https://spooky-alien-35919.herokuapp.com/api/filter/',{
         Institute,
         Year,
         Semester,
